30be04ce07e24c3ddfe7d71159d38e8dfa30fdd7
Madison Treat <madison.treat@tamu.edu>
2015-06-09 17:21:35 -0500

    Added QFI - QFlightInstruments by parekcel.pl/index.php?pg=qfi

--- README.md
+++ README.md
@@ -2,3 +2,10 @@ cpdisplay
 =========
 
 Cockpit Head Down Display
+
+
+Dependencies
+============
+* CMake (cmake)
+* Qt5 (qt5-default)
+* Qt5 Svg (libqt5svg5 libqt5svg5-dev)
--- hdd/gui/main.cpp
+++ hdd/gui/main.cpp
@@ -1,18 +1,20 @@
 #include <QApplication>
 #include <QDesktopWidget>
-#include "window.h"
+//#include "window.h"
+#include "MainWindow.h"
 
 int main(int argc, char* argv[]) {
+//   Q_INIT_RESOURCE(qfi);
    QApplication app(argc, argv);
    // Desktop size
    int deskWidth = QApplication::desktop()->width();
    int deskHeight = QApplication::desktop()->height();
    
-   HDDWindow window;
-   // Window size
-   int windowWidth = window.width();
-   int windowHeight = window.height();
-
+//   HDDWindow window;
+//   // Window size
+//   int windowWidth = window.width();
+//   int windowHeight = window.height();
+   MainWindow window;
 
    window.showMaximized();
 
